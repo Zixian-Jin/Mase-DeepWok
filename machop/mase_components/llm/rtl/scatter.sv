f8c3a7e94d8a915c8fd251f5641f9a787b1afe63
Zixian Jin <119010134@link.cuhk.edu.cn>
2024-03-26 20:00:43 +0000

    Removed mask.sv. ind_table signal is replaced by internal wires in scatter.sv

--- machop/mase_components/llm/rtl/scatter.sv
+++ machop/mase_components/llm/rtl/scatter.sv
@@ -25,32 +25,11 @@ module scatter #(
     output data_out_valid,
     input data_out_ready
 );
-
-    logic [0 :0] ind_table        [IN_SIZE * IN_PARALLELISM-1 :0];
-    logic [IN_WIDTH-1: 0] reg_data_in      [IN_SIZE * IN_PARALLELISM-1: 0];
-    logic reg_data_in_valid;
-    
-    mask #(
-        .IN_WIDTH (IN_WIDTH),
-        .IN_SIZE (IN_SIZE),
-        .IN_PARALLELISM (IN_PARALLELISM)
-    ) mask_inst(
-        .clk (clk),
-        .rst (rst),
-        .data_in (data_in),
-        .data_in_valid (data_in_valid),
-        .data_in_ready (data_in_ready),
-        .ind_table (ind_table),
-        .data_out (reg_data_in),
-        .data_out_valid (reg_data_in_valid),
-        .data_out_ready (data_out_ready)
-    );
-        
-    // Assumes that if a large number occurs at the first row, then the rest entries in that column are all large numbers
+    // Check all entries of the input matrix
     for (genvar i = 0; i < IN_PARALLELISM; i = i + 1) begin: ROW 
         // Parse flattened data_in
         logic [IN_WIDTH-1 :0] current_data_in [IN_SIZE-1 :0];
-        assign current_data_in = reg_data_in[IN_SIZE*(i+1)-1 :IN_SIZE*i];
+        assign current_data_in = data_in[IN_SIZE*(i+1)-1 :IN_SIZE*i];
         
         // Parse flattened data_out
         logic [IN_WIDTH-1 :0] current_data_out_large [IN_SIZE-1 :0];
@@ -59,8 +38,17 @@ module scatter #(
         assign data_out_small[IN_SIZE*(i+1)-1 :IN_SIZE*i] = current_data_out_small[IN_SIZE-1 :0];
 
         for (genvar j = 0; j < IN_SIZE; j = j + 1) begin: COL
+
+            logic is_outlier;
+            fp16_comparator #(
+                .IN_WIDTH (IN_WIDTH)
+            ) fp16_cmp_inst (
+                .data_in (current_data_in[j]),
+                .result (is_outlier)
+            );  
+
             always_comb begin
-                if (ind_table[i*IN_SIZE + j] == 1'b0) begin
+                if (is_outlier == 1'b0) begin
                     // small number, allocated to small column
                     current_data_out_small[j] = current_data_in[j];
                     current_data_out_large[j] = 0;
@@ -88,16 +76,6 @@ module scatter #(
 
     end
 
-    // always_ff @(posedge clk) begin: RST
-    //     if (rst) begin
-    //         data_in_ready <= 0;
-    //         data_out_valid <= 0;
-    //     end else begin
-    //         data_in_ready <= 1;
-    //         data_out_valid <= 1;
-    //     end
-    // end
-    assign data_out_valid = !rst && reg_data_in_valid; //&& (ROW[0].counter == IN_SIZE);
-    // assign data_out_small = reg_out_small;
-    // assign data_out_large = reg_out_large;
+    assign data_out_valid = !rst && data_in_valid; //&& (ROW[0].counter == IN_SIZE);
+    assign data_in_ready = !rst && data_out_ready;
 endmodule
